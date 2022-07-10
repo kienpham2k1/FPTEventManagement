40618de3379288c673d1bedfcb1ebf036be16087
phuhung9292 <phuhungx1810@gmail.com>
2022-07-10 20:24:47 +0700

    login and regis

---
+++ frmLogin/RegistorForm.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace FptEventWinApp
+{
+    partial class RegistorForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtconfirmpass = new System.Windows.Forms.TextBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.txtphone = new System.Windows.Forms.TextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.gender = new System.Windows.Forms.GroupBox();
+            this.female = new System.Windows.Forms.RadioButton();
+            this.male = new System.Windows.Forms.RadioButton();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.gender.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel4.ForeColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(88, 213);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(233, 1);
+            this.panel4.TabIndex = 34;
+            // 
+            // txtconfirmpass
+            // 
+            this.txtconfirmpass.BackColor = System.Drawing.SystemColors.Control;
+            this.txtconfirmpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtconfirmpass.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtconfirmpass.Location = new System.Drawing.Point(88, 187);
+            this.txtconfirmpass.Name = "txtconfirmpass";
+            this.txtconfirmpass.Size = new System.Drawing.Size(125, 20);
+            this.txtconfirmpass.TabIndex = 33;
+            this.txtconfirmpass.Text = "Confirm Password";
+            this.txtconfirmpass.Click += new System.EventHandler(this.txtconfirmpass_Click);
+            // 
+            // p4
+            // 
+            this.p4.Image = global::FptEventWinApp.Properties.Resources.login__1_;
+            this.p4.Location = new System.Drawing.Point(45, 187);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(37, 33);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p4.TabIndex = 32;
+            this.p4.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.panel2.Location = new System.Drawing.Point(88, 135);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(232, 1);
+            this.panel2.TabIndex = 31;
+            // 
+            // txtemail
+            // 
+            this.txtemail.BackColor = System.Drawing.SystemColors.Control;
+            this.txtemail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtemail.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtemail.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtemail.Location = new System.Drawing.Point(88, 109);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(230, 20);
+            this.txtemail.TabIndex = 30;
+            this.txtemail.Text = "Email";
+            this.txtemail.Click += new System.EventHandler(this.txtemail_Click);
+            this.txtemail.Leave += new System.EventHandler(this.txtemail_Leave);
+            // 
+            // p2
+            // 
+            this.p2.Image = global::FptEventWinApp.Properties.Resources.mail;
+            this.p2.Location = new System.Drawing.Point(44, 109);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(38, 30);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p2.TabIndex = 29;
+            this.p2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::FptEventWinApp.Properties.Resources.stamped;
+            this.pictureBox6.Location = new System.Drawing.Point(142, 3);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(87, 59);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 28;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 27;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel3.ForeColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(87, 174);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(233, 1);
+            this.panel3.TabIndex = 26;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BackColor = System.Drawing.SystemColors.Control;
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpassword.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtpassword.Location = new System.Drawing.Point(88, 148);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(125, 20);
+            this.txtpassword.TabIndex = 25;
+            this.txtpassword.Text = "Password";
+            this.txtpassword.Click += new System.EventHandler(this.txtpassword_Click);
+            // 
+            // p3
+            // 
+            this.p3.Image = global::FptEventWinApp.Properties.Resources.password__1_1;
+            this.p3.Location = new System.Drawing.Point(45, 148);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(37, 33);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p3.TabIndex = 24;
+            this.p3.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.panel1.Location = new System.Drawing.Point(85, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 1);
+            this.panel1.TabIndex = 23;
+            // 
+            // p1
+            // 
+            this.p1.Image = global::FptEventWinApp.Properties.Resources.user__1_;
+            this.p1.Location = new System.Drawing.Point(44, 60);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(37, 36);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p1.TabIndex = 22;
+            this.p1.TabStop = false;
+            // 
+            // txtusername
+            // 
+            this.txtusername.BackColor = System.Drawing.SystemColors.Control;
+            this.txtusername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtusername.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtusername.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.txtusername.Location = new System.Drawing.Point(85, 76);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(230, 20);
+            this.txtusername.TabIndex = 21;
+            this.txtusername.Text = "User Name";
+            this.txtusername.Click += new System.EventHandler(this.txtusername_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkCyan;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(88, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 30);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Registor";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // p5
+            // 
+            this.p5.Image = global::FptEventWinApp.Properties.Resources.phone_call;
+            this.p5.Location = new System.Drawing.Point(44, 226);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(37, 36);
+            this.p5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.p5.TabIndex = 36;
+            this.p5.TabStop = false;
+            // 
+            // txtphone
+            // 
+            this.txtphone.BackColor = System.Drawing.SystemColors.Control;
+            this.txtphone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtphone.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtphone.Location = new System.Drawing.Point(88, 226);
+            this.txtphone.Name = "txtphone";
+            this.txtphone.Size = new System.Drawing.Size(125, 20);
+            this.txtphone.TabIndex = 37;
+            this.txtphone.Text = "Phone Number";
+            this.txtphone.Click += new System.EventHandler(this.txtphone_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel5.ForeColor = System.Drawing.Color.Black;
+            this.panel5.Location = new System.Drawing.Point(88, 252);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(233, 1);
+            this.panel5.TabIndex = 35;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::FptEventWinApp.Properties.Resources.gender_symbols;
+            this.pictureBox2.Location = new System.Drawing.Point(45, 268);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(37, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 38;
+            this.pictureBox2.TabStop = false;
+            // 
+            // gender
+            // 
+            this.gender.Controls.Add(this.female);
+            this.gender.Controls.Add(this.male);
+            this.gender.Location = new System.Drawing.Point(88, 255);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(248, 56);
+            this.gender.TabIndex = 39;
+            this.gender.TabStop = false;
+            this.gender.Text = "Gender";
+            // 
+            // female
+            // 
+            this.female.AutoSize = true;
+            this.female.Checked = true;
+            this.female.Location = new System.Drawing.Point(154, 26);
+            this.female.Name = "female";
+            this.female.Size = new System.Drawing.Size(78, 24);
+            this.female.TabIndex = 1;
+            this.female.TabStop = true;
+            this.female.Text = "Female";
+            this.female.UseVisualStyleBackColor = true;
+            // 
+            // male
+            // 
+            this.male.AutoSize = true;
+            this.male.Location = new System.Drawing.Point(9, 27);
+            this.male.Name = "male";
+            this.male.Size = new System.Drawing.Size(63, 24);
+            this.male.TabIndex = 0;
+            this.male.TabStop = true;
+            this.male.Text = "Male";
+            this.male.UseVisualStyleBackColor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // RegistorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.txtphone);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.txtconfirmpass);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.txtusername);
+            this.Name = "RegistorForm";
+            this.Size = new System.Drawing.Size(370, 369);
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.gender.ResumeLayout(false);
+            this.gender.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtconfirmpass;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.TextBox txtphone;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox gender;
+        private System.Windows.Forms.RadioButton female;
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
